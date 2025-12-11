6d5bde45c047e5dced367b67bd16d805a6ea71b2
hagai240700 <yhagai240700@gmail.com>
2025-12-11 13:50:04 +0200

    commit to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,17 @@
-import React from "react"; // ייבוא React
-import ReactDOM from "react-dom/client"; // ייבוא ReactDOM ליצירת root
-import App from "./App.tsx"; // ייבוא קומפוננטת השורש App
-import "./index.css"; // ייבוא עיצוב כללי
-import { BrowserRouter } from "react-router-dom"; // ייבוא BrowserRouter לראוטינג
+import React from "react"; // מייבא את React כדי להשתמש ב-JSX
+import ReactDOM from "react-dom/client"; // מייבא את ReactDOM ליצירת root בדפדפן
+import App from "./App.tsx"; // מייבא את קומפוננטת השורש App
+import "./index.css"; // מייבא את קובץ ה-CSS הראשי של האפליקציה
+import { BrowserRouter } from "react-router-dom"; // מייבא את BrowserRouter כדי לאפשר ראוטינג באפליקציה
 
 ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement // יצירת root מתוך האלמנט עם id="root"
+  document.getElementById("root") as HTMLElement // יוצר root מתוך האלמנט עם id="root" בקובץ index.html
 ).render(
-  <React.StrictMode> 
-    <BrowserRouter> 
-      <App /> 
-    </BrowserRouter>
-  </React.StrictMode>
-);
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {/* StrictMode עוזר לזהות בעיות פוטנציאליות בזמן פיתוח */}
+    <BrowserRouter>
+      {/* BrowserRouter עוטף את האפליקציה ומאפשר שימוש בנתיבים (Routes) */}
+      <App /> {/* מציג את קומפוננטת השורש של האפליקציה */}
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <main style={{ padding: "20px" }}>
+      <h2>ברוך הבא לאפליקציית מנהל המשימות</h2>
+      <p>בחר עמוד מהתפריט כדי להתחיל לעבוד.</p>
+    </main>
+  );
+};
+
+export default Home;
