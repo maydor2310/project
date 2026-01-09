550eae25535933274024706a66a2d98e5a13961c
yehonatan hagai <yhagai240700@gmail.com>
2026-01-09 10:50:06 +0200

    commit main

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,21 @@
-import React from "react"; // comment: react import
-import ReactDOM from "react-dom/client"; // comment: react dom root
-import { BrowserRouter } from "react-router-dom"; // comment: router wrapper
-import { ThemeProvider } from "@mui/material/styles"; // comment: theme provider
-import CssBaseline from "@mui/material/CssBaseline"; // comment: normalize css
-import App from "./App"; // comment: app component
-import theme from "./theme/theme"; // comment: custom theme
-import "./index.css"; // comment: global css
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import App from "./App";
+import getTheme from "./theme/theme"; // 👈 זה factory, לא theme
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render( // comment: create root
-  <React.StrictMode> {/* comment: strict mode */}
-    <BrowserRouter> {/* comment: router wrapper */}
-      <ThemeProvider theme={theme}> {/* comment: provide MUI theme */}
-        <CssBaseline /> {/* comment: baseline styles */}
-        <App /> {/* comment: render app */}
+// 👇 יוצרים אובייקט theme אמיתי
+const theme = getTheme("light");
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
